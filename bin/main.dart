3a41d508ca1f813a664bc2a7f554fa489a8a4aa6
Daniel de Lima <dlima@tre-pa.jus.br>
2020-05-11 21:43:42 -0300

    Métodos com retornos

--- bin/main.dart
+++ bin/main.dart
@@ -3,16 +3,18 @@ void main() {
 
   contaAmanda.titular = "Amanda";
 
-  double saque = 180;
+  double saque = 80;
 
-  if (contaAmanda.saldo - saque < -100) {
-    print("sem saldo insuficiente");
-  } else {
-    print("sacando");
-    contaAmanda.saldo -= saque;
+  bool sucesso = contaAmanda.saque(saque);
+
+  if (sucesso) {
+    print("sucesso");
   }
-
   print("saldo da ${contaAmanda.titular}: ${contaAmanda.saldo}");
+
+  double saldo_atual = contaAmanda.deposito(200.0);
+
+  print("saldo da ${contaAmanda.titular}: $saldo_atual");
 }
 
 class ContaCorrente {
@@ -21,4 +23,22 @@ class ContaCorrente {
   int conta;
   //valor padrão do saldo deve ser zero
   double saldo = 20.0;
+
+  //método de saque
+  bool saque(double valorSaque) {
+    if (this.saldo - valorSaque < -100) {
+      print("sem saldo insuficiente");
+      return false;
+    } else {
+      print("sacando");
+      this.saldo -= valorSaque;
+      return true;
+    }
+  }
+
+  double deposito(double valor) {
+    print("depositando");
+    this.saldo += valor;
+    return this.saldo;
+  }
 }
