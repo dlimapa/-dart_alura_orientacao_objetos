4fde45198843283d20189d0b79edfbf02e594c2c
Daniel de Lima <dlima@tre-pa.jus.br>
2020-05-11 22:09:05 -0300

    Métodos chamando métodos

--- bin/main.dart
+++ bin/main.dart
@@ -15,6 +15,15 @@ void main() {
   double saldo_atual = contaAmanda.deposito(200.0);
 
   print("saldo da ${contaAmanda.titular}: $saldo_atual");
+
+  ContaCorrente contaTiago = ContaCorrente();
+
+  contaTiago.titular = "Tiago";
+
+  bool sucessoTransferencia = contaAmanda.transferencia(20.0, contaTiago);
+
+  print("saldo da ${contaAmanda.titular}: ${contaAmanda.saldo}");
+  print("saldo da ${contaTiago.titular}: ${contaTiago.saldo}");
 }
 
 class ContaCorrente {
@@ -23,22 +32,43 @@ class ContaCorrente {
   int conta;
   //valor padrão do saldo deve ser zero
   double saldo = 20.0;
+  double chequeEspecial = -100;
 
   //método de saque
   bool saque(double valorSaque) {
-    if (this.saldo - valorSaque < -100) {
-      print("sem saldo insuficiente");
+    if (!verificaSaldoSuficiente(valorSaque)) {
       return false;
     } else {
-      print("sacando");
+      print("sacando $valorSaque");
       this.saldo -= valorSaque;
       return true;
     }
   }
 
   double deposito(double valor) {
-    print("depositando");
+    print("depositando $valor");
     this.saldo += valor;
     return this.saldo;
+  }
+
+  bool transferencia(double valor, ContaCorrente contaDestino) {
+    if (!verificaSaldoSuficiente(valor)) {
+      return false;
+    } else {
+      print("transferindo");
+      this.saldo -= valor;
+      contaDestino.deposito(valor);
+      return true;
+    }
+  }
+
+  bool verificaSaldoSuficiente(double valor) {
+    if (this.saldo - valor < chequeEspecial) {
+      print("sem saldo suficiente");
+      return false;
+    } else {
+      print("movimentando $valor");
+      return true;
+    }
   }
 }
